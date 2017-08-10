1515500aebd5aa9f114042b55a96185350b1c4fa
root <root@localhost.localdomain>
2017-08-10 14:41:19 +0100

    update from kali

--- Client/clientTest.c
+++ Client/clientTest.c
@@ -2,27 +2,56 @@
 #include <stdio.h>
 #include <string.h>
 
-//test the connection is working
-void testConection(secureConnection con) {
-    char* id = (char*)"user1";
+void writeSC(secureConnection con) {
 
-    //use connection
-    char* name = (char*)calloc(20, 1);
-    for (unsigned int i = 0; i < strlen(id) + 1; i++) {
-      name[i] = id[i];
-    }
-    //source and target are the same
-    secureWrite(con, name, 20);
-    secureWrite(con, name, 20);
+      char* id = (char*)"user1";
 
-    char size = 26;
-    secureWrite(con, &size, 1);
+      //use connection
+      char* name = (char*)calloc(20, 1);
+      for (unsigned int i = 0; i < strlen(id) + 1; i++) {
+        name[i] = id[i];
+      }
+      //source and target are the same
+      secureWrite(con, name, 20);
+      secureWrite(con, name, 20);
 
-    char* data = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    secureWrite(con, data, 26);
+      char size = 26;
+      secureWrite(con, &size, 1);
+
+      char* data = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      secureWrite(con, data, 26);
 
 }
 
+void readSC(secureConnection con) {
+
+
+    printf("Received mail:\n");
+  	//read source
+    char src[20];
+    secureRead(con, src, 20);
+    printf("Sender: %s\n", src);
+
+    //read size
+    char size;
+    secureRead(con, &size, 20);
+    printf("Size: %d bytes\n", size);
+
+    //read data
+    char* data = (char*)malloc(size);
+    secureRead(con, data, size);
+    printf("Data: %s\n", data);
+}
+
+//test the connection is working
+void testConection(secureConnection con) {
+
+  writeSC(con);
+  readSC(con);
+
+
+
+}
 
 
 int main() {
--- Server/server.c
+++ Server/server.c
@@ -1,15 +1,16 @@
 
 #include "secSock.h"
 #include <stdio.h>
+#include <string.h>
 
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-//test the ssl connection
-void testConnection(secureConnection con) {
+void readSC(secureConnection con, char* src)  {
 
-  //read source
-  char src[20];
-  secureRead(con, src, 20);
-  printf("Source: %s\n", src);
 
   //read target
   char dst[20];
@@ -26,7 +27,101 @@ void testConnection(secureConnection con) {
   secureRead(con, data, size);
   printf("Data: %s\n", data);
 
-  //server response
+  //write file to inbox
+
+  //open file
+  char path[30];
+  sprintf(path, "%s/inbox.txt", dst);
+  int target_inbox = open(path, O_APPEND|O_CREAT, S_IRUSR|S_IWUSR);
+  if (target_inbox < 0) {
+	  perror("Failed to write to inbox.");
+	  exit(0);
+  }
+
+  //write sender
+  int len = 20;
+  //TODO check return value type
+  if (write(target_inbox, src, len) != len) {
+	  perror("Unable to write to file.");
+	  exit(0);
+  }
+
+  //write size
+  //TODO check return value type
+  len = 1;
+  if (write(target_inbox, &size, len) != len) {
+	  perror("Unable to write to file.");
+	  exit(0);
+  }
+
+  //write data
+  len = size;
+  if (write(target_inbox, data, len) != len) {
+	  perror("Unable to write to file.");
+	  exit(0);
+  }
+}
+
+void writeSC(secureConnection con, char* src) {
+
+    //server response
+    char path[30];
+    sprintf(path, "%s/inbox.txt", src);
+    int target_inbox = open(path, O_APPEND|O_CREAT, S_IRUSR|S_IWUSR);
+    if (target_inbox < 0) {
+  	  perror("Failed to open inbox.");
+  	  exit(0);
+    }
+
+    //read sender
+    int len = 20;
+    char sender[20];
+    //TODO check return value type
+    if (read(target_inbox, sender, len) != len) {
+  	  perror("Unable to read from file.");
+  	  exit(0);
+    }
+
+    //read size
+    char msgSize;
+    len = 1;
+    //TODO check return value type
+    if (read(target_inbox, &msgSize, len) != len) {
+  	  perror("Unable to read from file.");
+  	  exit(0);
+    }
+
+    //read data
+    len = msgSize;
+    char* msg = (char*)malloc(len);
+    if (read(target_inbox, msg, len) != len) {
+  	  perror("Unable to read from file.");
+  	  exit(0);
+    }
+
+      //send sender
+      secureWrite(con, sender, 20);
+
+  	//send size
+      secureWrite(con, &msgSize, 1);
+
+  	//send data
+      secureWrite(con, msg, len);
+
+}
+
+//test the ssl connection
+void testConnection(secureConnection con) {
+  //read source
+  char src[20];
+  secureRead(con, src, 20);
+  printf("Source: %s\n", src);
+
+  readSC(con, src);
+  writeSC(con, src);
+
+
+
 
   //clean up and end process
   closeConnection(con);
